37d0d8050dbaae10be94d94a97d6551bbea0cd06
Liam Taylor <liam.taylor888@googlemail.com>
2016-11-28 19:31:18 +0000

    1) Created failing test for the storing of trnaactions details and timestamp to a log hash 2) Created transactions_log hash to pass the failing test

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,20 @@
 
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions_log
 
   def initialize
     @balance = 0
+    @transactions_log = Hash.new
   end
 
   def make_transaction(amount)
     @balance += amount
+    @transactions_log[format_time] = amount
+  end
+
+  def format_time
+    Time.new.strftime("%d-%b-%Y")
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,14 +4,19 @@ describe Account do
 
   subject { described_class.new }
 
+
   describe '#initialization' do
     it 'initializes with zero balance' do
       expect(subject.balance).to eq 0
+    end
+    it 'initializes an emtpy transactions hash' do
+      expect(subject.transactions_log).to eq Hash.new
     end
   end
 
   describe '#make_transaction' do
     before do
+      allow(subject).to receive(:format_time) {"05-01-2017"}
       subject.make_transaction(5)
     end
     it 'applies a credit to the balance when passed a positive integer' do
@@ -22,5 +27,10 @@ describe Account do
       subject.make_transaction(-5)
       expect(subject.balance).to eq 0
     end
+    it 'pushes the transaction and a timestamp to the transactions log hash' do
+      subject.make_transaction(5)
+      expect(subject.transactions_log).to include({'05-01-2017'=>5})
+    end
   end
+
 end
